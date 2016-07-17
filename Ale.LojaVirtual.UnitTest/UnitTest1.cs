1f5a1432873985f710193dfea01e9d39b620760c
alejc <alejc@DESKTOP-GN3VDKU>
2016-07-17 17:25:05 -0300

    Add project files.

---
+++ Ale.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ale.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestAle
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
